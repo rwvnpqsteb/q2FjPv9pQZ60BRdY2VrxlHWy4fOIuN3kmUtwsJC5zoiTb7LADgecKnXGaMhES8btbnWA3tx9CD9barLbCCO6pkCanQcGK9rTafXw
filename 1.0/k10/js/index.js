beda7f3dec2085631be5f91d57b7fa5ee5412112
rwvnpqsteb <teguhsahestu10@gmail.com>
2022-11-20 18:43:44 +0700

    file

---
+++ 1.0/k10/js/index.js
@@ -0,0 +1,34 @@
+var url;
+var kunciurl = 'true';
+
+if (localStorage.getItem('url')) {
+    url = localStorage.getItem('url');
+} else {
+    url = 'https://cbt-smkmigas.com/';
+}
+
+if (url) {
+    document.getElementById('url').value = url;
+} else {
+    document.getElementById('url').value = '';
+}
+
+function run() {
+    if (kunciurl === "true") {
+        url = "https://cbt-smkmigas.com/";
+    } else {
+        url = document.getElementById('url').value;
+    }
+    
+    localStorage.setItem('url',url);
+
+    document.getElementById("content").innerHTML = '<iframe id="iframe" src="'+url+'?examkey=wHCVItIDQ0l7cwM50h4AYe5DTgHxWdEcmQtGoAqIJgNE7HkTBQoyxlYV1W3V70aLcU5jz4XKj2tnYRa9Ewp4mVyxMGnECtiIg1lU" frameborder="0"></iframe>';
+
+}
+
+setTimeout(function() {
+    if (kunciurl === "true") {
+        run()
+    }
+    document.getElementById('bg').style.display = 'none';
+}, 3000);
